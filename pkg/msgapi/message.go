2c2406f6487bdd33aef415bdd00bf9fb8dab068d
Sergey Semernin <serpsem@proton.me>
2026-01-03 21:02:50 +0100

    Use of the configured per area character set to decode a message.

--- pkg/msgapi/message.go
+++ pkg/msgapi/message.go
@@ -131,6 +131,9 @@ func (m *Message) Encode() {
 // Decode charset
 func (m *Message) Decode() {
 	enc := strings.Split(config.Config.Chrs.Default, " ")[0]
+	if (*m.AreaObject).GetChrs() != "" {
+		enc = strings.Split((*m.AreaObject).GetChrs(), " ")[0]
+	}
 	if _, ok := m.Kludges["CHRS"]; ok {
 		enc = m.Kludges["CHRS"]
 		if enc == "IBMPC" {
